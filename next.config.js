cd64b9ebc2a007e64effdcef4064d6ec1cd72002
Khanh Luu <luukhanh96@gmail.com>
2026-07-06 23:13:14 -0400

    update layout

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
   // Turbopack configuration for Next.js 16+
